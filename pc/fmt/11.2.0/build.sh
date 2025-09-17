a4332632b1bd85ec60c6da6c96aa9063f3f4fc49
gubaojian <gubaojian@163.com>
2025-09-17 18:07:27 +0800

    fmt库

---
+++ pc/fmt/11.2.0/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh build_fmt.sh
+sh build_fmt_debug.sh
---
+++ pc/fmt/11.2.0/build_fmt_debug.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LIB_MODE=debug
+LIB_NAME=fmt
+VERSION=11.2.0
+INSTALL_DIR=$HOME/cdeps/${LIB_MODE}/${LIB_NAME}/${VERSION}
+CDEPS_ZIP_FILE=cdeps_${LIB_NAME}_${VERSION}.zip
+DOWNLOAD_FILE=${LIB_NAME}-${VERSION}.tar.gz
+DOWNLOAD_URL="https://github.com/fmtlib/fmt/archive/refs/tags/${VERSION}.tar.gz"
+UNZIP_DIR=${LIB_NAME}-${VERSION}
+uname -a
+echo "Script directory: $SCRIPT_DIR"
+echo "INSTALL directory: $INSTALL_DIR"
+echo "CDEPS_ZIP_FILE: $CDEPS_ZIP_FILE"
+echo "DOWNLOAD_URL: $DOWNLOAD_URL"
+echo "UNZIP_DIR: $UNZIP_DIR"
+
+rm -rf ${CDEPS_ZIP_FILE}
+
+echo "Checking download file: ${DOWNLOAD_FILE}"
+file_path="$SCRIPT_DIR/${DOWNLOAD_FILE}"
+if [ -f "$file_path" ]; then
+  echo "$file_path file already exists, skipping download."
+else
+  echo "$file_path not exist, try download file"
+  rm -rf ${DOWNLOAD_FILE}
+  curl -OL ${DOWNLOAD_URL}
+fi
+rm -rf ${UNZIP_DIR}
+echo "Extracting ${LIB_NAME} source code to ${UNZIP_DIR} ..."
+tar -zxvf ${DOWNLOAD_FILE}
+cd ${UNZIP_DIR}
+rm -rf ${INSTALL_DIR}
+mkdir -p ${INSTALL_DIR}
+export CFLAGS="${CFLAGS} -g -O0 "
+export CXXFLAGS="${CXXFLAGS} -g -O0 "
+
+##  -DBUILD_TESTING=OFF
+ mkdir build
+ cd build
+ cmake -DCMAKE_BUILD_TYPE=Debug -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
+ make -j2
+ make install
+ cd ..
+ 
+cd ..
+rm -rf ${CDEPS_ZIP_FILE}
+cd $HOME/cdeps/${LIB_MODE}/${LIB_NAME}/
+rm -rf ${CDEPS_ZIP_FILE}
+zip -r ${CDEPS_ZIP_FILE} ${VERSION}
+echo "${CDEPS_ZIP_FILE} packaging completed."
+mv ${CDEPS_ZIP_FILE} "$SCRIPT_DIR/${CDEPS_ZIP_FILE}"
+echo "Package moved to script directory: $SCRIPT_DIR/${CDEPS_ZIP_FILE}"
+
+cd $SCRIPT_DIR
+rm -rf $HOME/cdeps/src/${LIB_NAME}/${VERSION}/
+mkdir -p $HOME/cdeps/src/${LIB_NAME}/${VERSION}/
+mv ${UNZIP_DIR} $HOME/cdeps/src/${LIB_NAME}/${VERSION}/
+echo "move compile source code to $HOME/cdeps/src/${LIB_NAME}/${VERSION}/ "
+echo "you can recompile it later, in this folder $HOME/cdeps/src/${LIB_NAME}/${VERSION}/ "
+
+echo "${LIB_NAME} Build script completed successfully."
+
+
+
+
+
